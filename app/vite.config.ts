3130ebd9b38ddb9cbebbef472857462e64a0da2d
lm16688 <121564098+lm16688@users.noreply.github.com>
2026-02-05 13:08:42 +0800

    Add files via upload

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -12,9 +12,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  root: 'app', // 添加这一行，指定根目录为 app 文件夹
-  build: {
-    outDir: '../dist', // 可选：如果你希望构建输出在根目录的 dist 文件夹中
-    emptyOutDir: true
-  }
 });
